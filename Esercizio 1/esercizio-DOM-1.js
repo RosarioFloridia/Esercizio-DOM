0429ef5638ede99b53ae969e0ba20bec4699be3b
Rosario Floridia <rosariofloridia35@gmail.com>
2026-05-17 14:44:03 +0200

    test

---
+++ Esercizio 1/esercizio-DOM-1.js
@@ -0,0 +1,10 @@
+const ul = document.querySelector("ul");
+        const secondoFiglioUl = ul.children[1];
+
+        console.log(ul.parentElement);
+
+        console.log(ul.children[1]);
+
+        console.log(secondoFiglioUl.nextElementSibling);
+        
+        console.log(secondoFiglioUl.previousElementSibling);
---
+++ Esercizio 2/esercizio-DOM-2.js
@@ -0,0 +1,3 @@
+const p = document.getElementsByClassName(".paragrafo")
+const elementoTre = document.getElementById("#elemento-3")
+const h2 = document.querySelectorAll("h2")
