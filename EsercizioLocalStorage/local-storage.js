b4e8358be757a31881abda670e12a500e6545762
Rosario Floridia <rosariofloridia35@gmail.com>
2026-05-19 11:24:30 +0200

    fixes

---
+++ EsercizioLocalStorage/local-storage.js
@@ -0,0 +1,5 @@
+const dataEmail = document.querySelector(".email");
+const dataFirstName = document.querySelector(".firstname");
+const dataLastName = document.querySelector(".lastname");
+const bottone = document.querySelector(".btn")
+const title = document.querySelector(".title-name")
